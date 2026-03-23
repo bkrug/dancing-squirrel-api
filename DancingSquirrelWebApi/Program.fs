2af495f7c56f7cf8108a699369b7aa213bd5b920
Benjamin Krug <benjaminkrug@yahoo.com>
2026-03-23 09:55:19 -0400

    Enable Cors

--- DancingSquirrelWebApi/Program.fs
+++ DancingSquirrelWebApi/Program.fs
@@ -1,11 +1,53 @@
 open Falco
 open Falco.Routing
+open System
+open System.Collections.Generic
+open System.IO
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore
 open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.HttpsPolicy
+open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.Logging
 
-let wapp = WebApplication.Create()
+let allowedOriginsPolicy = "DancingSquirrelOrigins"
 
-wapp.UseRouting()
-    .UseFalco([
-        get "/" (Response.ofPlainText "Hello World!")
-    ])
-    .Run(Response.ofPlainText "Not found")
+let wbuilder = WebApplication.CreateBuilder()
+
+wbuilder.Services.AddCors(fun options ->
+    options.AddPolicy(
+        allowedOriginsPolicy,
+        fun policy -> policy.WithOrigins("http://localhost:3000").AllowAnyHeader().AllowAnyMethod() |> ignore
+    ) |> ignore
+) |> ignore
+
+let wapp = wbuilder.Build()
+
+type trainingRequest =
+    {
+        CaretakerName: string
+    }
+
+let helloWorldHandler : HttpHandler = fun ctx ->
+    task {
+        let! f : FormData = Request.getForm ctx
+        let person =
+            { CaretakerName = f.GetString ("caretakername", "") }
+        let name = "Nameless One"
+        let message = sprintf "Hello %s" name
+        return! Response.ofPlainText message ctx
+    }
+
+let endpoints =
+    [
+        post "/api/request/create" helloWorldHandler
+    ]
+
+wapp.UseRouting() |> ignore
+wapp.UseCors(allowedOriginsPolicy) |> ignore
+wapp.UseFalco(endpoints)
+    .Run(Response.withStatusCode 404 >> Response.ofPlainText "Not found")
