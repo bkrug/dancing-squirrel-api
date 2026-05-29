71310a6fb0c6a869f6dd31a55ea6df35e85b4c32
Benjamin Krug <benjaminkrug@yahoo.com>
2026-05-29 15:12:45 -0400

    Remove dead code

--- DancingSquirrelWebApi/ExternalDependencies.fs
+++ DancingSquirrelWebApi/ExternalDependencies.fs
@@ -2,13 +2,4 @@ module ExternalDependencies
 
 open DbLayer
 
-type IGetDb =
-    abstract member GetDb: unit -> Database.QueryContextFactory
-
-type DbGetter(connStr) =
-    interface IGetDb with
-        member this.GetDb (): Database.QueryContextFactory = 
-            let db = Database.QueryContextFactory.Create(connStr, printfn "SQL: %O")
-            db
-
 let getDbContextFactory connStr = Database.QueryContextFactory.Create(connStr, printfn "SQL: %O")
