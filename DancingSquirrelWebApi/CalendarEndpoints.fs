2008e080c87e718427a2f8ee4567dd6e91323ce1
Benjamin Krug <benjaminkrug@yahoo.com>
2026-09-08 23:40:42 -0400

    Refactor

--- DancingSquirrelWebApi/CalendarEndpoints.fs
+++ DancingSquirrelWebApi/CalendarEndpoints.fs
@@ -1,6 +1,5 @@
 module Calendar.Endpoints
 
-open DbLayer.Database
 open DbLayer.Database.main
 open Falco
 open GenericModels
@@ -11,8 +10,6 @@ open System.Text.RegularExpressions
 open System.Threading.Tasks
 open Calendar.Models
 open Calendar.Queries
-open Microsoft.AspNetCore.Authentication
-open Microsoft.AspNetCore.Authentication.Cookies
 open ValidationStandards
 
 //Todo: Move into named constant
@@ -38,63 +35,62 @@ let getParsedTimeOfDay (timeOfDayOption: Option<string>) =
         | _ ->
             Error "Must be in the format 'hh:mm'"
 
-let parseToDefaultAvailability (form: CreateEditDefaultAvailability) : Result<list<DefaultAvailability>, DefaultAvailabilityValidation> =
-    let validatedRows = 
-        form.Availabilities
-        |> Seq.map (fun a ->
-            let dayOfWeekR = getParsedDayOfWeek a.DayOfWeek
-            let startTimeR = getParsedTimeOfDay a.StartTime
-            let endTimeR = getParsedTimeOfDay a.EndTime
-            let dbA =
-                match dayOfWeekR, startTimeR, endTimeR with
-                | Ok dayOfWeek, Ok startTime, Ok endTime ->
-                    Ok {
-                        TeacherId = a.TeacherId;
-                        DayOfWeek = dayOfWeek;
-                        StartTimeUnix = startTime;
-                        EndTimeUnix = endTime;
-                        DefaultAvailabilityId = 0;
-                    }
-                | _ ->
-                    let validations: DefaultDayAvailabilityValidation =
-                        {
-                            TeacherId = "";
-                            DayOfWeek = match dayOfWeekR with | Error msg -> msg | _ -> "";
-                            StartTime = match startTimeR with | Error msg -> msg | _ -> "";
-                            EndTime = match endTimeR with | Error msg -> msg | _ -> "";
-                        }
-                    Error validations
-            dbA
-        )
-    let emptySeq : seq<DefaultAvailability> = []
-    let successOrFail =
-        (Ok emptySeq, validatedRows)
-        ||> Seq.fold (fun acc rowParseResult ->
-            match acc, rowParseResult with
-            | Error prevErrors, Error newError ->
-                Error (Seq.append prevErrors [newError])
-            | Error prevErrors, Ok _ ->
-                Error (Seq.append prevErrors [ { TeacherId = ""; DayOfWeek = ""; StartTime = ""; EndTime = ""; } ])
-            | Ok prevSuccess, Error newError ->
-                Error (
-                    seq {
-                        for i in 1..Seq.length prevSuccess do
-                            yield { TeacherId = ""; DayOfWeek = ""; StartTime = ""; EndTime = ""; }
-                        yield newError
-                    }
-                )
-            | Ok prevSuccess, Ok newSuccess ->
-                Ok (Seq.append prevSuccess [newSuccess])
-        )
-    match successOrFail with
-        | Ok success -> Ok (Seq.toList success)
-        | Error err -> Error { Availabilities = Seq.toArray err }
-
 let createDefaultAvailabilityFromForm
     (form: CreateEditDefaultAvailability)
     (upsertRecords: list<DefaultAvailability> -> Task<Result<list<DefaultAvailability>, RecordInsertError>>) =
     task {
-        match parseToDefaultAvailability form with
+        let validatedRows = 
+            form.Availabilities
+            |> Seq.map (fun a ->
+                let dayOfWeekR = getParsedDayOfWeek a.DayOfWeek
+                let startTimeR = getParsedTimeOfDay a.StartTime
+                let endTimeR = getParsedTimeOfDay a.EndTime
+                let dbA =
+                    match dayOfWeekR, startTimeR, endTimeR with
+                    | Ok dayOfWeek, Ok startTime, Ok endTime ->
+                        Ok {
+                            TeacherId = a.TeacherId;
+                            DayOfWeek = dayOfWeek;
+                            StartTimeUnix = startTime;
+                            EndTimeUnix = endTime;
+                            DefaultAvailabilityId = 0;
+                        }
+                    | _ ->
+                        let validations: DefaultDayAvailabilityValidation =
+                            {
+                                TeacherId = "";
+                                DayOfWeek = match dayOfWeekR with | Error msg -> msg | _ -> "";
+                                StartTime = match startTimeR with | Error msg -> msg | _ -> "";
+                                EndTime = match endTimeR with | Error msg -> msg | _ -> "";
+                            }
+                        Error validations
+                dbA
+            )
+        let emptySeq : seq<DefaultAvailability> = []
+        let successOrFail =
+            (Ok emptySeq, validatedRows)
+            ||> Seq.fold (fun acc rowParseResult ->
+                match acc, rowParseResult with
+                | Error prevErrors, Error newError ->
+                    Error (Seq.append prevErrors [newError])
+                | Error prevErrors, Ok _ ->
+                    Error (Seq.append prevErrors [ { TeacherId = ""; DayOfWeek = ""; StartTime = ""; EndTime = ""; } ])
+                | Ok prevSuccess, Error newError ->
+                    Error (
+                        seq {
+                            for i in 1..Seq.length prevSuccess do
+                                yield { TeacherId = ""; DayOfWeek = ""; StartTime = ""; EndTime = ""; }
+                            yield newError
+                        }
+                    )
+                | Ok prevSuccess, Ok newSuccess ->
+                    Ok (Seq.append prevSuccess [newSuccess])
+            )
+        let listVal = 
+            match successOrFail with
+            | Ok success -> Ok (Seq.toList success)
+            | Error err -> Error { Availabilities = Seq.toArray err }        
+        match listVal with
         | Error validation ->
             return Error (getGenericValidationFailure validation)
         | Ok parsedData ->
