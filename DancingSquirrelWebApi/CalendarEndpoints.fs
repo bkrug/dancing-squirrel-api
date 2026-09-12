73f20fe33a828199ec972ab0a125a99bda0db19f
Benjamin Krug <benjaminkrug@yahoo.com>
2026-09-12 19:07:03 -0400

    Refactor

--- DancingSquirrelWebApi/CalendarEndpoints.fs
+++ DancingSquirrelWebApi/CalendarEndpoints.fs
@@ -8,34 +8,28 @@ open Calendar.Models
 open Calendar.Queries
 open ValidationStandards
 
-let private getRequiredResult (inputValue : Option<string>) =
+let private parseRequiredString inputValue =
     match inputValue |> Extensions.emptyStringToNone with
-        | Some nonEmpty -> Ok nonEmpty
-        | None -> Error requiredMessage
+    | Some nonEmpty -> Ok nonEmpty
+    | None -> Error requiredMessage
 
-let private getParsedDayOfWeek (dayOfWeekOption: Option<string>) =
-    getRequiredResult dayOfWeekOption
-    |> Result.bind (fun dayOfWeek ->
-        match Extensions.tryParseEnum<System.DayOfWeek> dayOfWeek with
-        | Some dayValue -> Ok(int64 dayValue)
-        | None -> Error "Must be Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, or Sunday"
-    )
+let private parseDayOfWeek dayOfWeek =
+    match Extensions.tryParseEnum<System.DayOfWeek> dayOfWeek with
+    | Some dayValue -> Ok(int64 dayValue)
+    | None -> Error "Must be Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, or Sunday"
 
-let private getParsedTimeOfDay (timeOfDayOption: Option<string>) =
-    getRequiredResult timeOfDayOption
-    |> Result.bind (fun timeOfDay ->
-        match Extensions.tryParseTimeOnly timeOfDay with
-        | Some parsedTime -> Ok (int64(parsedTime.ToTimeSpan().TotalSeconds))
-        | None -> Error "Must be in the format 'hh:mm'"
-    )
+let private parseTimeOfDay timeOfDay =
+    match Extensions.tryParseTimeOnly timeOfDay with
+    | Some parsedTime -> Ok (int64(parsedTime.ToTimeSpan().TotalSeconds))
+    | None -> Error "Must be in the format 'hh:mm'"
 
 let private emptyRowValidation : DefaultDayAvailabilityValidation =
     { TeacherId = ""; DayOfWeek = ""; StartTime = ""; EndTime = "" }
 
 let private parseRow (row: CreateEditDefaultDayAvailability) : Result<DefaultAvailability, DefaultDayAvailabilityValidation> =
-    let dayOfWeekR = getParsedDayOfWeek row.DayOfWeek
-    let startTimeR = getParsedTimeOfDay row.StartTime
-    let endTimeR = getParsedTimeOfDay row.EndTime
+    let dayOfWeekR = parseRequiredString row.DayOfWeek |> Result.bind parseDayOfWeek
+    let startTimeR = parseRequiredString row.StartTime |> Result.bind parseTimeOfDay
+    let endTimeR = parseRequiredString row.EndTime |> Result.bind parseTimeOfDay
     match dayOfWeekR, startTimeR, endTimeR with
     | Ok dayOfWeek, Ok startTime, Ok endTime ->
         Ok {
