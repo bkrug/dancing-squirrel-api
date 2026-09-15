5b63432ed2b34ce0c9cc4b40b8162ad113af4e8c
Benjamin Krug <benjaminkrug@yahoo.com>
2026-09-15 13:46:11 -0400

    Add test for editing availabilities

--- DancingSquirrelTests/CreateDefaultAvailabilityTests.fs
+++ DancingSquirrelTests/CreateDefaultAvailabilityTests.fs
@@ -66,6 +66,8 @@ let ``Creating Default availabilities from form with entries for Monday through 
         //Assert
         submissionResult.IsOk.ShouldBeTrue()
         actualReceivedRecords.ShouldBeEquivalentTo(expectedRecords)
+        callBeginTransactions.ShouldBe(1)
+        callCommitTransactions.ShouldBe(1)
     }
 
 let defaultAvailabilityValidationFailureData : list<CreateEditDefaultDayAvailability * string * string> =
@@ -196,4 +198,83 @@ let ``Creating default availability that has a Wednesday entry that overlaps ano
         | Error errResp ->
             let dayValidation = errResp.ValidationFailures.Value.Availabilities.[2]
             dayValidation.StartTime.ShouldBeEquivalentTo("overlaps another availability period")
+    }
+
+
+[<Fact>]
+let ``Editing group of Default availabilities. Expect some records to be inserted, some to be updated, and some to be deleted.`` () =
+    task {
+        let teacherId = 53
+        let callerInput : CreateEditDefaultAvailability =
+            {
+                Availabilities = [|
+                    { DefaultAvailabilityId = None;      DayOfWeek = Some "Monday";    StartTime = Some "09:00:00"; EndTime = Some "17:00:00" }
+                    { DefaultAvailabilityId = Some 1001; DayOfWeek = Some "Tuesday";   StartTime = Some "09:00:00"; EndTime = Some "17:00:00" }
+                    { DefaultAvailabilityId = Some 1002; DayOfWeek = Some "WEDNESDAY"; StartTime = Some "09:00:00"; EndTime = Some "17:00:00" }
+                    { DefaultAvailabilityId = None;      DayOfWeek = Some "thursday";  StartTime = Some "09:00:00"; EndTime = Some "17:00:00" }
+                    { DefaultAvailabilityId = None;      DayOfWeek = Some "SaturDay";  StartTime = Some "10:00:00"; EndTime = Some "14:00:00" }
+                |]
+            }
+        let currentDbRecords =
+            [|
+                { DayOfWeek = int64 DayOfWeek.Tuesday;   StartTimeUnix = getUnixSeconds  9 30; EndTimeUnix = getUnixSeconds 17  0; DefaultAvailabilityId = 1001; TeacherId = teacherId; }
+                { DayOfWeek = int64 DayOfWeek.Wednesday; StartTimeUnix = getUnixSeconds  9 30; EndTimeUnix = getUnixSeconds 17  0; DefaultAvailabilityId = 1002; TeacherId = teacherId; }
+                { DayOfWeek = int64 DayOfWeek.Wednesday; StartTimeUnix = getUnixSeconds  8 45; EndTimeUnix = getUnixSeconds 16 45; DefaultAvailabilityId = 2003; TeacherId = teacherId; }
+                { DayOfWeek = int64 DayOfWeek.Wednesday; StartTimeUnix = getUnixSeconds  9 15; EndTimeUnix = getUnixSeconds 17 15; DefaultAvailabilityId = 2004; TeacherId = teacherId; }
+            |]
+            |> Seq.toList
+        let expectedInserts =
+            [|
+                { DayOfWeek = int64 DayOfWeek.Monday;    StartTimeUnix = getUnixSeconds  9 0; EndTimeUnix = getUnixSeconds 17 0; DefaultAvailabilityId = 0; TeacherId = teacherId; }
+                { DayOfWeek = int64 DayOfWeek.Thursday;  StartTimeUnix = getUnixSeconds  9 0; EndTimeUnix = getUnixSeconds 17 0; DefaultAvailabilityId = 0; TeacherId = teacherId; }
+                { DayOfWeek = int64 DayOfWeek.Saturday;  StartTimeUnix = getUnixSeconds 10 0; EndTimeUnix = getUnixSeconds 14 0; DefaultAvailabilityId = 0; TeacherId = teacherId; }
+            |]
+            |> Seq.toList
+        let expectedUpdates =
+            [|
+                { DayOfWeek = int64 DayOfWeek.Tuesday;   StartTimeUnix = getUnixSeconds  9 0; EndTimeUnix = getUnixSeconds 17 0; DefaultAvailabilityId = 1001; TeacherId = teacherId; }
+                { DayOfWeek = int64 DayOfWeek.Wednesday; StartTimeUnix = getUnixSeconds  9 0; EndTimeUnix = getUnixSeconds 17 0; DefaultAvailabilityId = 1002; TeacherId = teacherId; }
+            |]
+            |> Seq.toList
+        let expectedDeletes = [| 2003, 2004 |] |> Seq.toList
+
+        let mutable actualInsertedRecords : list<DefaultAvailability> = []
+        let mutable actualUpdatedRecords : list<DefaultAvailability> = []
+        let mutable actualDeletedRecords : list<int> = []
+        let mutable recordId = 3000
+        let mutable callBeginTransactions = 0
+        let mutable callCommitTransactions = 0
+
+        let fakeQueries =
+            { new ICalendarQueries with
+                member _.BeginTransactionAsync =
+                    callBeginTransactions <- callBeginTransactions + 1
+                    Task.FromResult()
+                member _.CommitTransaction =
+                    callCommitTransactions <- callCommitTransactions + 1
+                member _.GetDefaultAvailabilityAsync givenTeacherId = 
+                    givenTeacherId.ShouldBe(teacherId)
+                    Task.FromResult(currentDbRecords)
+                member _.InsertDefaultAvailability recordToInsert =
+                    actualInsertedRecords <- actualInsertedRecords @ [ recordToInsert ]
+                    recordId <- recordId + 1
+                    Task.FromResult(Ok recordId)
+                member _.UpdateDefaultAvailability recordToUpdate =
+                    actualUpdatedRecords <- actualUpdatedRecords @ [ recordToUpdate ]
+                    Task.FromResult(Ok())
+                member _.DeleteDefaultAvailability recordId =
+                    actualDeletedRecords <- actualDeletedRecords @ [ recordId ]
+                    Task.FromResult(Ok())
+            }
+
+        //Act
+        let! submissionResult = editDefaultAvailabilityFromForm callerInput teacherId fakeQueries
+
+        //Assert
+        submissionResult.IsOk.ShouldBeTrue()
+        actualInsertedRecords.ShouldBeEquivalentTo(expectedInserts)
+        actualUpdatedRecords.ShouldBeEquivalentTo(expectedUpdates)
+        actualDeletedRecords.ShouldBeEquivalentTo(expectedDeletes)
+        callBeginTransactions.ShouldBe(1)
+        callCommitTransactions.ShouldBe(1)
     }
